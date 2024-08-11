a5bb0ff22d32c23ff7c33fb5fd15327f83512e16
daniel watson <ozzloy@each.do>
2024-08-10 20:39:43 -0700

    use AST to find new comment lines and blocks

--- test/02-code-comments-spec.js
+++ test/02-code-comments-spec.js
@@ -2,9 +2,17 @@ const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-spies'));
 const fs = require('fs');
+const parser = require('@babel/parser');
 
-describe('02-code-comments.js', function() {
+describe('02-code-comments.js', function () {
   let consoleSpy;
+  const problemPath = __dirname + '/../problems/02-code-comments.js';
+  const fileContent = fs.readFileSync(problemPath, 'utf-8');
+  const codeCommentsAST = parser.parse(fileContent, {
+    sourceType: 'module',
+    tokens: true,
+  });
+
   before(() => {
     consoleSpy = chai.spy.on(console, 'log');
     require('../problems/02-code-comments');
@@ -19,20 +27,39 @@ describe('02-code-comments.js', function() {
   });
 
   it('added a single-line comment', function() {
-    const re = /^\/\//;
-    const linesMatched = fs.readFileSync(__dirname + '/../problems/02-code-comments.js', 'utf-8')
-      .split('\n')
-      .map(line => line.trim())
-      .filter(line => re.test(line));
-    expect(linesMatched.length).to.eq(4);
+    const originalLineComments = [
+      " 2. This is a single line comment.",
+      " console.log('This is will not be printed to the terminal')",
+      " Try adding your own single line comments:",
+      " Your code here",
+    ];
+
+    const newCommentLines = codeCommentsAST.comments.filter(comment =>
+      comment.type === 'CommentLine'
+        && !originalLineComments.includes(comment.value)
+    );
+
+    expect(newCommentLines.length, 'No new single-line comment found')
+      .to.be.at.least(1);
   });
 
   it('added a multi-line comment', function() {
-    const re = /\/\*/;
-    const linesMatched = fs.readFileSync(__dirname + '/../problems/02-code-comments.js', 'utf-8')
-      .split('\n')
-      .map(line => line.trim())
-      .filter(line => re.test(line));
-    expect(linesMatched.length).to.eq(5);
+    const originalMultiLineComments = [
+      '\n'
+        + 'Use your newfound knowledge of comments to execute the prompts in this problem\n'
+        + 'file.\n',
+      ' 1. This will print \'Welcome to App Academy!\'  ',
+      ' Print more messages to the terminal using console.log ',
+      ' 3.\n'
+        + 'This is a multi-line comment.\n'
+        + 'Try adding your own multi-line comments:\n',
+    ];
+    const newCommentBlocks = codeCommentsAST.comments.filter(comment =>
+      comment.type === 'CommentBlock'
+        && !originalMultiLineComments.includes(comment.value)
+    );
+
+    expect(newCommentBlocks.length, 'No new multi-line comment found')
+      .to.be.at.least(1);
   });
 });
